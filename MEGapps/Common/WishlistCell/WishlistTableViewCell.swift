4c9dc5976f10dcac414b3c537ba603a39fbff9ce
Arya <aryailham05@gmail.com>
2021-10-26 22:20:18 +0700

    ADD: WishlistTableViewCell

---
+++ MEGapps/Common/WishlistCell/WishlistTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  WishlistTableViewCell.swift
+//  MEGapps
+//
+//  Created by Arya Ilham on 26/10/21.
+//
+
+import UIKit
+
+@IBDesignable class WishlistTableViewCell: UITableViewCell {
+    // MARK: - IBOutlet
+    @IBOutlet weak var statusIconImg: UIImageView!
+    @IBOutlet weak var itemNameLbl: UILabel!
+    @IBOutlet weak var waitingEndDateLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        setupUI()
+    }
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    override func prepareForReuse() {
+        itemNameLbl.text = "ItemName"
+        waitingEndDateLbl.text = "Date"
+        statusIconImg.image = UIImage(systemName: "clock")
+    }
+    // MARK: - UI Function
+    func setupUI() {
+        self.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
+    }
+
+}
