391c258032dbba470c61b7fcf190b8313d09e8bc
Arya <aryailham05@gmail.com>
2021-10-26 12:51:03 +0700

    Initial commit

---
+++ MEGapps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MEGapps
+//
+//  Created by Arya Ilham on 26/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
