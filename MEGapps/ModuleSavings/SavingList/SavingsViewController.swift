64c6c37fb2cdb978e200b92000a88a1a153e711e
William Giovanni Kambuno <wpalalangan@gmail.com>
2021-10-26 23:10:41 +0700

    ADD: HobbySavingCell Custom UIView; Plugged into Savings View

---
+++ MEGapps/ModuleSavings/SavingList/SavingsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SavingsViewController.swift
+//  MEGapps
+//
+//  Created by William Giovanni Kambuno on 26/10/21.
+//
+
+import UIKit
+
+class SavingsViewController: UIViewController {
+
+//MARK: - Outlets
+    @IBOutlet weak var viewHobbySavingsCell: HobbySavingsCellView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
