c381b0f01948772179b8caf0c436729a8acec3cd
Arya <aryailham05@gmail.com>
2021-10-27 12:57:25 +0700

    ADD: Shadow to wishlist cell

--- MEGapps/Common/WishlistCell/WishlistTableViewCell.swift
+++ MEGapps/Common/WishlistCell/WishlistTableViewCell.swift
@@ -12,9 +12,11 @@ class WishlistTableViewCell: UITableViewCell {
     @IBOutlet weak var statusIconImg: UIImageView!
     @IBOutlet weak var itemNameLbl: UILabel!
     @IBOutlet weak var waitingEndDateLbl: UILabel!
+    @IBOutlet weak var wishlistContainerView: UIView!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        setupUI()
     }
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
@@ -25,5 +27,12 @@ class WishlistTableViewCell: UITableViewCell {
         itemNameLbl.text = "ItemName"
         waitingEndDateLbl.text = "Date"
         statusIconImg.image = UIImage(systemName: "clock")
+    }
+    func setupUI() {
+        wishlistContainerView.layer.shadowColor = UIColor.black.cgColor
+        wishlistContainerView.layer.shadowRadius = 2
+        wishlistContainerView.layer.shadowOpacity = 0.3
+        wishlistContainerView.layer.shadowOffset = CGSize(width: 0, height: 3)
+        wishlistContainerView.layer.masksToBounds = false
     }
 }
