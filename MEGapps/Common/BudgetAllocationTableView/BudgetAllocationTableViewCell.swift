616a8a35c4aec7e8862b9fe5df87c1de6cc0ac9f
Adinda Puji R <adindapujir@gmail.com>
2021-10-28 16:46:20 +0700

    Add Purchase overview Storyboard and Budget Allocation XIB
    Storyboard Purchase overview
    Table View Purchase overview
    XIB Budget Allocation
    Table View Budget Allocation

---
+++ MEGapps/Common/BudgetAllocationTableView/BudgetAllocationTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  BudgetAllocationTableViewCell.swift
+//  MEGapps
+//
+//  Created by Adinda Puji Rahmawaty on 28/10/21.
+//
+
+import UIKit
+
+class BudgetAllocationTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var budgetName: UILabel!
+    @IBOutlet weak var budgetAmount: UILabel!
+    @IBOutlet weak var budgetUsedAmt: UILabel!
+    @IBOutlet weak var budgetLeftAmt: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
