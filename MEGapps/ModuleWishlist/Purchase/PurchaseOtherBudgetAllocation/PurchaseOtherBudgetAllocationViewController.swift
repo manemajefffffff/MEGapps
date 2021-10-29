07861900b89e1e893477ddabf2d528e672cde250
Arya <aryailham05@gmail.com>
2021-10-29 11:07:54 +0700

    CREATE: Purchase other budget allocation controller and storyboard

---
+++ MEGapps/ModuleWishlist/Purchase/PurchaseOtherBudgetAllocation/PurchaseOtherBudgetAllocationViewController.swift
@@ -0,0 +1,54 @@
+//
+//  PurchaseOtherBudgetAllocationViewController.swift
+//  MEGapps
+//
+//  Created by Arya Ilham on 29/10/21.
+//
+
+import UIKit
+
+class PurchaseOtherBudgetAllocationViewController: UIViewController {
+    // MARK: - IBOutlet
+    @IBOutlet weak var otherBudgetAllocationTableView: UITableView!
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        registerNib()
+    }
+    // MARK: - Function
+    private func registerNib() {
+        otherBudgetAllocationTableView.register(UINib.init(nibName: "PurchaseAllocateBudgetTableViewCell", bundle: nil), forCellReuseIdentifier: "PurchaseAllocateBudgetTableViewCell")
+    }
+    private func setupTableDropShadow() {
+        let containerView: UIView = UIView(frame: self.otherBudgetAllocationTableView.frame)
+        containerView.backgroundColor = UIColor.clear
+        containerView.layer.shadowColor = UIColor.lightGray.cgColor
+        containerView.layer.shadowOffset = CGSize(width: 0, height: 5)
+        containerView.layer.shadowOpacity = 0.5
+        containerView.layer.shadowRadius = 2
+
+        otherBudgetAllocationTableView.layer.cornerRadius = 10
+        otherBudgetAllocationTableView.layer.masksToBounds = true
+        self.view.addSubview(containerView)
+        containerView.addSubview(otherBudgetAllocationTableView)
+    }
+}
+
+extension PurchaseOtherBudgetAllocationViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = otherBudgetAllocationTableView
+                .dequeueReusableCell(
+                    withIdentifier: "PurchaseAllocateBudgetTableViewCell", for: indexPath)
+                as? PurchaseAllocateBudgetTableViewCell else {
+                    fatalError("no table cell found")
+                }
+        
+        return cell
+    }
+    
+    
+}
