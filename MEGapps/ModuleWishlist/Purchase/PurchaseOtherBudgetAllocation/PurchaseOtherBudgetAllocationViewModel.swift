0838285dae5007b4577bf4ceb1835ec657a913c7
Arya <aryailham05@gmail.com>
2021-10-29 22:52:22 +0700

    Create PurchaseOtherBudgetAllocationViewModel.swift

---
+++ MEGapps/ModuleWishlist/Purchase/PurchaseOtherBudgetAllocation/PurchaseOtherBudgetAllocationViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  PurchaseOtherBudgetAllocationViewModel.swift
+//  MEGapps
+//
+//  Created by Arya Ilham on 29/10/21.
+//
+
+import Foundation
+
+class PurchaseOtherBudgetAllocationViewModel {
+    let bundle: Bundle
+    
+    init(bundle: Bundle = Bundle.main) {
+        self.bundle = bundle
+    }
+}
