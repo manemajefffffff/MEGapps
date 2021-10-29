fe847be055d8e391329d9d873c65c518ea3d2bca
William Giovanni Kambuno <wpalalangan@gmail.com>
2021-10-29 22:15:28 +0700

    ADD: PurchaseSameItem Storyboard & TableView.

---
+++ MEGapps/Common/PurchaseSameItemCell/PurchaseSameItemTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  PurchaseSameItemTableViewCell.swift
+//  MEGapps
+//
+//  Created by William Giovanni Kambuno on 29/10/21.
+//
+
+import UIKit
+
+class PurchaseSameItemTableViewCell: UITableViewCell {
+    
+//MARK: - Outlets
+    @IBOutlet weak var labelProductName: UILabel!
+    @IBOutlet weak var labelProductDeadline: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
