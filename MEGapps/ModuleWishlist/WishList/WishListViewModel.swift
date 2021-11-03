7e53ee511afabf3f459f246eddd06f7ba45d833f
Arya <aryailham05@gmail.com>
2021-11-03 13:33:19 +0700

    Update WishListViewModel.swift

--- MEGapps/ModuleWishlist/WishList/WishListViewModel.swift
+++ MEGapps/ModuleWishlist/WishList/WishListViewModel.swift
@@ -20,12 +20,51 @@ class WishListViewModel {
     
     @Published var items = [ItemModel]()
     
-    private var anyCancellable = Set<AnyCancellable>()
+//    private var anyCancellable = Set<AnyCancellable>()
     
+    private lazy var wishlistCDM: WishlistCoreDataManager = {return WishlistCoreDataManager() }()
+
     init() {}
     // MARK: - Function
     func fetchData() {
         // pastikan kalau kosong
         items.removeAll()
+//        items.append(
+//            ItemModel(
+//                itemID: UUID(),
+//                name: "test nama",
+//                reason: "test alasan",
+//                status: "test status",
+//                category: "test kategori",
+//                price: 100004,
+//                deadline: Date(),
+//                startSavingDate: Date(),
+//                purchaseDate: Date(),
+//                createdAt: Date(),
+//                isPrioritize: false
+//            )
+//        )
+        self.wishlistCDM.get { itemList in
+            self.items = itemList
+        }
+        
+    }
+    
+    func addNewData(){
+        items.append(
+                    ItemModel(
+                        itemID: UUID(),
+                        name: "test nama",
+                        reason: "test alasan",
+                        status: "test status",
+                        category: "test kategori",
+                        price: 100004,
+                        deadline: Date(),
+                        startSavingDate: Date(),
+                        purchaseDate: Date(),
+                        createdAt: Date(),
+                        isPrioritize: false
+                    )
+                )
     }
 }
