f8b5ee177d031a4b0fad3579fd5fb68018053861
Arya <aryailham05@gmail.com>
2021-11-03 16:09:55 +0700

    Update WishListViewModel.swift

--- MEGapps/ModuleWishlist/WishList/WishListViewModel.swift
+++ MEGapps/ModuleWishlist/WishList/WishListViewModel.swift
@@ -8,20 +8,10 @@
 import Foundation
 import Combine
 
-class WishListViewModel {
+class WishListViewModel: ObservableObject {
     
-//    private var dataFromCoreData: WishListModel? {
-//        didSet {
-//            self.bindWishListViewModeltoController()
-//        }
-//    }
-    
-//    var bindWishListViewModeltoController: (() -> Void) = {}
-    
-    @Published var items = [ItemModel]()
-    
-//    private var anyCancellable = Set<AnyCancellable>()
-    
+    @Published var items = [Items]()
+        
     private lazy var wishlistCDM: WishlistCoreDataManager = {return WishlistCoreDataManager() }()
 
     init() {}
@@ -29,42 +19,8 @@ class WishListViewModel {
     func fetchData() {
         // pastikan kalau kosong
         items.removeAll()
-//        items.append(
-//            ItemModel(
-//                itemID: UUID(),
-//                name: "test nama",
-//                reason: "test alasan",
-//                status: "test status",
-//                category: "test kategori",
-//                price: 100004,
-//                deadline: Date(),
-//                startSavingDate: Date(),
-//                purchaseDate: Date(),
-//                createdAt: Date(),
-//                isPrioritize: false
-//            )
-//        )
         self.wishlistCDM.get { itemList in
             self.items = itemList
         }
-        
-    }
-    
-    func addNewData(){
-        items.append(
-                    ItemModel(
-                        itemID: UUID(),
-                        name: "test nama",
-                        reason: "test alasan",
-                        status: "test status",
-                        category: "test kategori",
-                        price: 100004,
-                        deadline: Date(),
-                        startSavingDate: Date(),
-                        purchaseDate: Date(),
-                        createdAt: Date(),
-                        isPrioritize: false
-                    )
-                )
     }
 }
