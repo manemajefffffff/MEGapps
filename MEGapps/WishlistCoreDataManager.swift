a144d30af0e134da601e22783c94f8d0323331cd
Arya <aryailham05@gmail.com>
2021-11-04 14:39:30 +0700

    Update WishlistCoreDataManager.swift

--- MEGapps/WishlistCoreDataManager.swift
+++ MEGapps/WishlistCoreDataManager.swift
@@ -13,39 +13,32 @@ class WishlistCoreDataManager {
     
     lazy var persistentContainer: NSPersistentContainer = {
         let container = NSPersistentContainer(name: "MEGapps")
-
         container.loadPersistentStores { _, error in
             guard error == nil else {
-                fatalError("\(error!)")
+                fatalError("Unresolved error \(error!)")
             }
         }
         container.viewContext.automaticallyMergesChangesFromParent = false
         container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
         container.viewContext.shouldDeleteInaccessibleFaults = true
         container.viewContext.undoManager = nil
-
         return container
     }()
     
     private func newTaskContext() -> NSManagedObjectContext {
         let taskContext = persistentContainer.newBackgroundContext()
         taskContext.undoManager = nil
-
         taskContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
         return taskContext
     }
     
-    func get(completion: @escaping(_ itemList: [Items]) -> Void) {
-        let context = newTaskContext()
-        
-        context.perform {
-//            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Items")
-            do {
-                let result = try context.fetch(Items.fetchRequest())
-                completion(result)
-            } catch {
-                print("error when fetching data")
-            }
+    func getAll(completion: @escaping(_ items: [Items]) -> Void) {
+        let context = persistentContainer.viewContext
+        do {
+            let data = try context.fetch(Items.fetchRequest())
+            completion(data)
+        } catch {
+            fatalError()
         }
-    }
+    }    
 }
