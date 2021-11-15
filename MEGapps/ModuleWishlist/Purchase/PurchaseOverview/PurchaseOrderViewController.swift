0893218fcd0ac6d943fef9712fbbc614add6a781
Adinda Puji R <adindapujir@gmail.com>
2021-11-15 14:04:27 +0700

    Fix UI Wishlist Overview
    - UI Storyboard sesuai
    - Belum ada process

--- MEGapps/ModuleWishlist/Purchase/PurchaseOverview/PurchaseOrderViewController.swift
+++ MEGapps/ModuleWishlist/Purchase/PurchaseOverview/PurchaseOrderViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class PurchaseOrderViewController: UIViewController, BudgetAllocationTVCProtocol, ProductInformationTVCProtocol {
+class PurchaseOrderViewController: UIViewController, ProductInformationTVCProtocol {
     func changepiHeight(piHeight: CGFloat) {
         DispatchQueue.main.async {
             self.productInfoHeight?.constant = piHeight
@@ -15,49 +15,41 @@ class PurchaseOrderViewController: UIViewController, BudgetAllocationTVCProtocol
         }
     }
     
-    func changeHeight(baHeight: CGFloat) {
-        DispatchQueue.main.async {
-            self.viewHeight?.constant = baHeight
-            self.allocBudgUiView.layoutIfNeeded()
-        }
-    }
-    
+    // MARK: Outlets
     @IBOutlet weak var productInfoHeight: NSLayoutConstraint!
-    @IBOutlet var viewHeight: NSLayoutConstraint!
-    @IBOutlet weak var allocBudgUiView: UIView!
     @IBOutlet weak var productInfoUiView: UIView!
     
+    // MARK: View
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    // MARK: Buttons
+    @IBAction func backButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func addToWishlistButton(_ sender: Any) {
+       addToWishlistPressed()
+    }
+    
+    @IBAction func cancelWishlistButton(_ sender: Any) {
+        cancelWishlistPressed()
+    }
+    
+    // MARK: Functions
+    func addToWishlistPressed() {
+        let alert = UIAlertController(title: "Add Item", message: "Are you sure you want to add this item?", preferredStyle: UIAlertController.Style.alert)
         
-        // Do any additional setup after loading the view.
-    }
-//    override func viewWillLayoutSubviews() {
-//        super.updateViewConstraints()
-//        self.productInfoHeight?.constant =
-//    }
-    
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    
-    
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        super.prepare(for: segue, sender: sender)
-        if segue.identifier == "allocToPurchase" {
-            guard let dest = segue.destination as? BudgetAllocationOverviewTableViewController
-            else {return}
-            dest.delegate = self
-            return
-        }
-//        if let childViewController = segue.destination as? BudgetAllocationOverviewTableViewController {
-//            childViewController.view.translatesAutoresizingMaskIntoConstraints = false
-//    }
-    
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Confirm", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
     
-    
+    func cancelWishlistPressed() {
+        let alert = UIAlertController(title: "Cancel Purchase", message: "Are you sure you want to cancel this item?", preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Confirm", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 }
