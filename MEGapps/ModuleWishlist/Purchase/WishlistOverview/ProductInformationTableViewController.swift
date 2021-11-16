e1793de32d41694e587faba01db9389f0e3d8ecf
Arya <aryailham05@gmail.com>
2021-11-16 12:15:00 +0700

    Update ProductInformationTableViewController.swift

--- MEGapps/ModuleWishlist/Purchase/WishlistOverview/ProductInformationTableViewController.swift
+++ MEGapps/ModuleWishlist/Purchase/WishlistOverview/ProductInformationTableViewController.swift
@@ -13,8 +13,21 @@ protocol ProductInformationTVCProtocol: AnyObject {
 
 class ProductInformationTableViewController: UITableViewController {
     
-    weak var delegate: ProductInformationTVCProtocol? = nil
-
+    // MARK: - IBOutlet
+    @IBOutlet weak var productNameLbl: UILabel!
+    @IBOutlet weak var productPriceLbl: UILabel!
+    @IBOutlet weak var categoryNameLbl: UILabel!
+    @IBOutlet weak var deadlineLbl: UILabel!
+    @IBOutlet weak var reasonLbl: UILabel!
+    
+    // MARK: - Variables
+    weak var delegate: ProductInformationTVCProtocol?
+    var item: WishlistAdd? {
+        didSet {
+            setDataToUI()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -22,6 +35,16 @@ class ProductInformationTableViewController: UITableViewController {
     override func viewWillLayoutSubviews() {
         super.updateViewConstraints()
         delegate?.changepiHeight(piHeight: self.tableView.contentSize.height)
+    }
+    
+    private func setDataToUI() {
+        if let currentData = item {
+            productNameLbl.text = currentData.name
+            productPriceLbl.text = "\(currentData.price)"
+            categoryNameLbl.text = currentData.category
+            deadlineLbl.text = "\(currentData.deadline)"
+            reasonLbl.text = currentData.reason
+        }
     }
 
     // MARK: - Table view data source
