660af22b614f0474d9d67296356ff9023d2f3133
Arya <aryailham05@gmail.com>
2021-11-21 00:48:50 +0700

    ADD: code to request data from coredatamanager

--- MEGapps/ModuleOtherBudget/BudgetList/OtherBudgetViewModel.swift
+++ MEGapps/ModuleOtherBudget/BudgetList/OtherBudgetViewModel.swift
@@ -10,15 +10,24 @@ import Combine
 
 class OtherBudgetViewModel: NSObject {
     
+    // MARK: - Publisher
     @Published var otherBudget = [Budget]()
-    
+    @Published var hasItem: Bool = false
+
     override init() {
         super.init()
         self.fetchData()
     }
     
     private func fetchData() {
-        
+        OtherBudgetCoreDataManager.shared.get { budgets in
+            if budgets.count > 0 {
+                self.otherBudget = budgets
+                self.hasItem = true
+            } else {
+                self.hasItem = false
+            }
+        }
     }
     
 }
