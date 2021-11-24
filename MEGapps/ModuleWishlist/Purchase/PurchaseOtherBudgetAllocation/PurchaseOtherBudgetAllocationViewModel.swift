dbfc6d5e56e62dd0c72e32fd79106c5eba46ae19
Arya <aryailham05@gmail.com>
2021-11-24 12:04:29 +0700

    Update PurchaseOtherBudgetAllocationViewModel.swift

--- MEGapps/ModuleWishlist/Purchase/PurchaseOtherBudgetAllocation/PurchaseOtherBudgetAllocationViewModel.swift
+++ MEGapps/ModuleWishlist/Purchase/PurchaseOtherBudgetAllocation/PurchaseOtherBudgetAllocationViewModel.swift
@@ -6,11 +6,59 @@
 //
 
 import Foundation
+import Combine
 
-class PurchaseOtherBudgetAllocationViewModel {
-    let bundle: Bundle
+class PurchaseOtherBudgetAllocationViewModel: NSObject {
     
-    init(bundle: Bundle = Bundle.main) {
-        self.bundle = bundle
+    // MARK: - Publishers
+    @Published var budgetAmountUsed: Int64 = 0
+    @Published var budgetUsage = [BudgetUsed]()
+    @Published var isBudgetAllocatedEnough = false
+
+    // MARK: - Variables
+    var insufficientAmount: Int64 = 0
+    
+    override init() {
+        super.init()
+        self.getBudget()
+        self.getBudgetAmountUsed()
+    }
+    
+    // MARK: - Function(Other budget allocation)
+    func getBudgetAmountUsed() {
+        var amountUsedTemp: Int64 = 0
+        for item in budgetUsage {
+            amountUsedTemp += item.amountUsed
+        }
+        budgetAmountUsed = amountUsedTemp
+    }
+    
+    func getBudget() {
+        OtherBudgetCoreDataManager.shared.get { budgets in
+            var allBudgetTemp: [BudgetUsed] = []
+            for item in budgets {
+                var allBudget = BudgetUsed()
+                allBudget.budget = item
+                allBudgetTemp.append(allBudget)
+            }
+            if allBudgetTemp.count > 0 {
+                self.budgetUsage = allBudgetTemp
+            }
+        }
+    }
+    
+    func setOtherBudgetUsageAmount(amountUsed: Int64, index: Int) {
+        budgetUsage[index].amountUsed = amountUsed
+    }
+    
+    func proceedWishlist() {
+        
+    }
+}
+
+extension PurchaseOtherBudgetAllocationViewModel {
+    // MARK: - Function (Use budgets page)
+    func addNewBudgetUse() {
+        
     }
 }
