4980639887800c4922ff8654f1695fad356355f7
Arya <aryailham05@gmail.com>
2021-11-25 15:24:36 +0700

    Update AllocateOtherBudgetView.swift

--- MEGapps/ModuleSavings/AllocateOtherBudget/AllocateOtherBudgetView.swift
+++ MEGapps/ModuleSavings/AllocateOtherBudget/AllocateOtherBudgetView.swift
@@ -7,39 +7,93 @@
 
 import Foundation
 import UIKit
+import Combine
 
 class AllocateOtherBudgetView: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-   
     // MARK: - Outlet
     @IBOutlet weak var insufficientAmountLabel: UILabel!
     @IBOutlet weak var usedBudgetLabel: UILabel!
-    
     @IBOutlet weak var allocateBudgetTableView: UITableView!
+    @IBOutlet weak var proceedWishlistButton: UIButton!
+    
+    // MARK: - Variables
+    var item: Items?
+    var insufficientAmount: Int64 = 9700000
+    
+    // MARK: - ViewModel
+    let viewModel = AllocateOtherBudgetViewModel()
+    var anyCancellable = Set<AnyCancellable>()
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         register()
+        subscribe()
+        sendReceivedDataToVM()
     }
     
     // MARK: - Function
-    func register() {
+    private func sendReceivedDataToVM() {
+        viewModel.item = item
+        viewModel.insufficientAmount = insufficientAmount
+    }
+    
+    private func subscribe() {
+        // subscribe untuk budget used
+        viewModel.$budgetUsed
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] _ in
+                DispatchQueue.main.async {
+                    self?.allocateBudgetTableView.reloadData()
+                }
+            }.store(in: &anyCancellable)
+        
+        // susbcribe untuk total jumlah budget lain yang terpakai
+        viewModel.$budgetAmountUsed
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] _ in
+                self?.setOtherBudgetUsedTotal()
+            }.store(in: &anyCancellable)
+        
+        viewModel.$isBudgetAllocatedEnough
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] _ in
+                self?.setProceedWishlistButtonState()
+            }.store(in: &anyCancellable)
+    }
+    
+    private func setOtherBudgetUsedTotal() {
+        usedBudgetLabel.text = "Rp. \(viewModel.budgetAmountUsed)"
+    }
+    
+    private func setProceedWishlistButtonState() {
+        proceedWishlistButton.isEnabled = viewModel.isBudgetAllocatedEnough
+    }
+    
+    private func register() {
         allocateBudgetTableView.register(UINib(nibName: "AllocateOtherBudgetTableViewCell", bundle: nil), forCellReuseIdentifier: "AllocateOtherBudgetTableViewCell")
     }
     
-    func addAlert() {
+    private func addAlert() {
         let alert = UIAlertController(title: "Proceed Wishlist", message: "Are you sure you want to proceed with this wishlist?", preferredStyle: .alert)
         
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         
-        alert.addAction(UIAlertAction(title: "Confirm", style: .default, handler: {action in
-            //move page to purchase overview
-            
+        alert.addAction(UIAlertAction(title: "Confirm", style: .default, handler: { _ in
+            // move page to purchase overview
             
         }))
         
         self.present(alert, animated: true, completion: nil)
+    }
+    
+    private func changeAmountWillUsed(amount: Int64, index: Int) {
+        viewModel.setOtherBudgetUsageAmount(amountUsed: amount, index: index)
+    }
+    
+    private func deleteBudgetUsed(index: Int) {
+        viewModel.deleteUsedBudget(index: index)
     }
     
     // MARK: - Action
@@ -56,8 +110,16 @@ class AllocateOtherBudgetView: UIViewController, UITableViewDelegate, UITableVie
     @objc func buttonAction(sender: UIButton) {
         print("button tap")
         let storyBoard = UIStoryboard(name: "UseBudgets", bundle: nil)
-        let useOtherBudgetVC = storyBoard.instantiateViewController(withIdentifier: "useBudgetsPage")
-        self.present(useOtherBudgetVC, animated: true)
+        guard let useOtherBudgetVC = storyBoard.instantiateViewController(withIdentifier: "useBudgetsPage") as? UseBudgetsView else {
+            fatalError()
+        }
+        useOtherBudgetVC.budgetUsed = self.viewModel.budgetUsed
+        useOtherBudgetVC.budgetNotUsed = self.viewModel.budgetNotUsed
+        useOtherBudgetVC.delegate = self
+        
+        let navController = UINavigationController(rootViewController: useOtherBudgetVC)
+        
+        self.present(navController, animated: true)
     }
     
 }
@@ -66,7 +128,11 @@ class AllocateOtherBudgetView: UIViewController, UITableViewDelegate, UITableVie
 extension AllocateOtherBudgetView {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        if section == 0 {
+            return viewModel.budgetUsed.count
+        } else {
+            return 1
+        }
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -79,17 +145,19 @@ extension AllocateOtherBudgetView {
             guard let cell = allocateBudgetTableView.dequeueReusableCell(withIdentifier: "AllocateOtherBudgetTableViewCell") as? AllocateOtherBudgetTableViewCell else {
                 fatalError("cell not found!")
             }
+            cell.id = indexPath.row
+            cell.budget = viewModel.budgetUsed[indexPath.row]
+            cell.changeAmoutBudgetUsed = self.changeAmountWillUsed(amount:index:)
+            cell.deleteBudgetUsed = self.deleteBudgetUsed(index:)
             
             return cell
         } else {
             guard let cell = allocateBudgetTableView.dequeueReusableCell(withIdentifier: "useOtherBudgetButton") as? ButtonCellView else {
                 fatalError("cell not found!")
             }
-            
             cell.useOtherBudgetButton.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
             return cell
         }
-        
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -102,6 +170,19 @@ extension AllocateOtherBudgetView {
         }
         
         return height
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        let selectedCell = tableView.cellForRow(at: indexPath) as? AllocateOtherBudgetTableViewCell
+        let amountWillUse = selectedCell?.budgetAmountToUse ?? 0
+        self.viewModel.setOtherBudgetUsageAmount(amountUsed: amountWillUse, index: indexPath.row)
+    }
+}
+
+extension AllocateOtherBudgetView: backToAllocateOtherBudgetView {
+    func sendBack(budgetUsed: [BudgetUsed], budgetNotUsed: [BudgetUsed]) {
+        self.viewModel.budgetUsed = budgetUsed
+        self.viewModel.budgetNotUsed = budgetNotUsed
     }
 }
 
