95b55d6c319cf7fdbe9356ff07b15d316ac417b7
Arya <aryailham05@gmail.com>
2021-11-25 15:25:42 +0700

    Update AllocateOtherBudgetTableViewCell.swift

--- MEGapps/Common/AllocateOtherBudgetCell/AllocateOtherBudgetTableViewCell.swift
+++ MEGapps/Common/AllocateOtherBudgetCell/AllocateOtherBudgetTableViewCell.swift
@@ -12,7 +12,22 @@ class AllocateOtherBudgetTableViewCell: UITableViewCell {
     // MARK: - Outlet
     @IBOutlet weak var budgetNameLabel: UILabel!
     @IBOutlet weak var budgetAvailabelLabel: UILabel!
+    @IBOutlet weak var budgetToUseTextField: UITextField!
     
+    // MARK: - Variable
+    var id: Int = -1
+    var budgetAmountToUse: Int64 = 0
+    var budget: BudgetUsed? {
+        didSet {
+            setData()
+        }
+    }
+    
+    // MARK: Closure
+    var changeAmoutBudgetUsed: ((_ amount: Int64, _ index: Int) -> Void)?
+    var deleteBudgetUsed: ((_ index: Int) -> Void)?
+
+
     // MARK: - Lifecycle
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -25,8 +40,28 @@ class AllocateOtherBudgetTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    override func prepareForReuse() {
+        budgetNameLabel.text = "Budget Name"
+        budgetAvailabelLabel.text = "Rp. 0s"
+    }
+   
+    // MARK: - IBAction
     @IBAction func deleteBudgetCell(_ sender: Any) {
         print("button delete tap")
+        deleteBudgetUsed!(id)
     }
     
+    @IBAction func changeAmountWillUsed(_ sender: Any) {
+        if let amount = Int64("\(budgetToUseTextField.text ?? "0")") {
+            self.budgetAmountToUse = amount
+            changeAmoutBudgetUsed!(self.budgetAmountToUse, id)
+        }
+    }
+    
+    // MARK: Function
+    private func setData() {
+        budgetNameLabel.text = "\(budget?.budget?.name ?? "Budget Name")"
+        budgetAvailabelLabel.text = "Rp. \(budget?.budget?.amount ?? 0)"
+        budgetToUseTextField.text = "\(budget?.amountUsed ?? 0)"
+    }
 }
