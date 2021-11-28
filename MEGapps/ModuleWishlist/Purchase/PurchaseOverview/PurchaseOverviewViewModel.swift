04cce52e8c027bbeddf063eab3c7d0e97bf3d15e
Tassja <Hannatassja@student.umn.ac.id>
2021-11-28 21:02:21 +0700

    Show data Purchase Overview

--- MEGapps/ModuleWishlist/Purchase/PurchaseOverview/PurchaseOverviewViewModel.swift
+++ MEGapps/ModuleWishlist/Purchase/PurchaseOverview/PurchaseOverviewViewModel.swift
@@ -11,10 +11,41 @@ class PurchaseOverviewViewModel {
     
     let bundle: Bundle
     
-    @Published var itemWantToBuy: Items?
-    @Published var budgetUsed: [BudgetUsed]?
+    @Published var itemWantToBuy: Items? {
+        didSet {
+            if let price = itemWantToBuy?.price {
+                savingAmount = price
+            }
+        }
+    }
+    @Published var otherBudgetAmount: Int64 = 0
+    @Published var savingAmount: Int64 = 0
+    
+    var budgetUsed: [BudgetUsed]? {
+        didSet {
+            calculateOtherBudgetAmount()
+            calculateSavingAmount()
+        }
+    }
     
     init(bundle: Bundle = Bundle.main) {
         self.bundle = bundle
+    }
+    
+    func calculateOtherBudgetAmount() {
+        
+        if let budgetCalculate = budgetUsed {
+            var temp: Int64 = 0
+            for object in budgetCalculate {
+                temp += object.amountUsed
+            }
+            otherBudgetAmount =  temp
+        }
+    }
+    
+    func calculateSavingAmount() {
+        if let price = itemWantToBuy?.price {
+            savingAmount = price - otherBudgetAmount
+        }
     }
 }
